7460dc4fce48f8f7a3a18b1cf1f1be41fc7781da
xukl <xu2k3l4@outlook.com>
2019-08-08 18:38:01 +0800

    增加功能：对失败请求的提示

--- upload/upload.js
+++ upload/upload.js
@@ -8,13 +8,14 @@ function upload()
 
 	xhr.upload.addEventListener("progress", function(e) {
 		if (e.lengthComputable)
-		{
-			var percentage = Math.round((e.loaded * 100) / e.total);
-			ctrl.value = percentage;
-		}
+			ctrl.value = Math.round((e.loaded * 100) / e.total);
 	}, false);
-	xhr.upload.addEventListener("load", function(e) {
-		ctrl.value = 100;
+	xhr.addEventListener("readystatechange", function(e) {
+		if (xhr.readyState == 4)
+			if (xhr.status == 200)
+				ctrl.value = 100;
+			else
+				alert(xhr.statusText + "\n" + xhr.responseText);
 	}, false);
 	xhr.open("POST", "./upload.sh?filename=" + encodeURI(file.name));
 	xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
