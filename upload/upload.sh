81f27783fc00b012e1df065beaef9e7471a045d7
xukl <xu2k3l4@outlook.com>
2019-08-08 18:37:31 +0800

    修复BUG

--- upload/upload.sh
+++ upload/upload.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 response() {
 	printf "HTTP/1.1 $1 $2\r\n\
-Content-Type: text/plain\r\n\
+Content-Type: text/plain; charset=utf-8\r\n\
 \r\n\
 $3"
 }
@@ -11,12 +11,14 @@ FILENAME=`sed 's/filename=//' <<< $QUERY_STRING`
 # security check
 if echo $FILENAME | grep -q "\/\|\.\.\|[[:cntrl:]]"
 then
-	response "400" "Bad Request" "Oops!"
+	response "400" "Bad Request" "Oops! Invalid file name. "
 	exit
 fi
 
 FILE="$FILE_ROOT/$FILENAME"
-if ( cat > "$FILE" )
+
+cat > "$FILE"
+if [ $? != 0 ]
 then
 	response "500" "Internal Server Error" "Oops! Error occurred. "
 	exit
