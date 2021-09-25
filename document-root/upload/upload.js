a4ba726bdec540221b02edae4bd4dd4639f8eaeb
xukl <xu2k3l4@outlook.com>
2021-09-25 21:43:39 +0800

    文件上传增加多选功能

--- document-root/upload/upload.js
+++ document-root/upload/upload.js
@@ -1,35 +1,53 @@
-const ctrl = document.getElementById("progress");
-const status_block = document.getElementById("status");
+const progress_area = document.querySelector(".progress-area");
+
+function create_status_line(filename)
+{
+	const line_template = `\
+		<div class="status-block"></div>
+		<progress class="progress-bar" value=0></progress>
+		<span class="filename"></span>`;
+	const line = document.createElement("div");
+	line.innerHTML = line_template;
+	line.querySelector(".filename").innerText = filename;
+	return line;
+}
 
 function upload()
 {
-	status_block.style.backgroundColor = "#ff0";
-	const reader = new FileReader();
-	const xhr = new XMLHttpRequest();
-	const file = document.getElementById("File").files[0];
+	for (let file of document.getElementById("File").files)
+	{
+		const reader = new FileReader();
+		const xhr = new XMLHttpRequest();
+		const status_line = create_status_line(file.name);
+		progress_area.append(status_line);
 
-	xhr.upload.addEventListener("progress", (e) => {
-		if (e.lengthComputable)
-			ctrl.value = Math.round((e.loaded * 100) / e.total);
-	}, false);
-	xhr.addEventListener("readystatechange", (e) => {
-		if (xhr.readyState === 4)
-			if (xhr.status === 200)
-			{
-				ctrl.value = 100;
-				status_block.style.backgroundColor="#0f0";
-			}
-			else
-				alert(xhr.statusText + "\n" + xhr.responseText);
-	}, false);
-	xhr.open("POST", "../upload.py?filename=" + encodeURI(file.name));
-	xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
-	reader.onload = (evt) => { xhr.send(evt.target.result); };
-	reader.readAsArrayBuffer(file);
+		const status_block = status_line.querySelector(".status-block");
+		const progress_bar = status_line.querySelector(".progress-bar");
+
+		status_block.style.backgroundColor = "#ff0";
+		xhr.upload.addEventListener("progress", (e) => {
+			if (e.lengthComputable)
+				progress_bar.value = e.loaded / e.total;
+		}, false);
+		xhr.addEventListener("readystatechange", (e) => {
+			if (xhr.readyState === 4)
+				if (xhr.status === 200)
+				{
+					progress_bar.value = 1;
+					status_block.style.backgroundColor = "#0f0";
+				}
+				else
+					alert(xhr.statusText + "\n" + xhr.responseText);
+		}, false);
+		xhr.open("POST", "../upload.py?filename=" + encodeURI(file.name));
+		xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
+		reader.onload = (evt) => { xhr.send(evt.target.result); };
+		reader.readAsArrayBuffer(file);
+	}
 }
 
 function clear_status()
 {
-	ctrl.value = 0;
-	status_block.style.backgroundColor = "#f00";
+	while (progress_area.firstChild)
+		progress_area.removeChild(progress_area.firstChild);
 }
