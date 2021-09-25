1427fa2edad45c3d86a34e3243826edf30562b0c
xukl <xu2k3l4@outlook.com>
2021-09-25 21:43:10 +0800

    格式小修改

--- document-root/text-clip/upload.js
+++ document-root/text-clip/upload.js
@@ -1,17 +1,17 @@
-var status_block = document.getElementById("status");
+const status_block = document.getElementById("status");
 
 function upload()
 {
-	var xhr = new XMLHttpRequest();
+	const xhr = new XMLHttpRequest();
 
-	xhr.addEventListener("readystatechange", function(e) {
-		if (xhr.readyState == 4)
-			if (xhr.status == 200)
-				status_block.style.backgroundColor="#0f0";
+	xhr.addEventListener("readystatechange", (e) => {
+		if (xhr.readyState === 4)
+			if (xhr.status === 200)
+				status_block.style.backgroundColor = "#0f0";
 			else
 				alert(xhr.statusText + "\n" + xhr.responseText);
 	}, false);
-	var text_title = document.getElementById("title").value || "untitled";
+	const text_title = document.getElementById("title").value || "untitled";
 	xhr.open("POST", "../upload.py?filename=" + encodeURI(text_title) + ".txt");
 	xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
 	xhr.send(document.getElementById("main-text").value);
@@ -19,5 +19,5 @@ function upload()
 
 function clear_status()
 {
-	status_block.style.backgroundColor="#f00";
+	status_block.style.backgroundColor = "#f00";
 }
--- document-root/upload/upload.js
+++ document-root/upload/upload.js
@@ -1,20 +1,20 @@
-var ctrl = document.getElementById("progress");
-var status_block = document.getElementById("status");
+const ctrl = document.getElementById("progress");
+const status_block = document.getElementById("status");
 
 function upload()
 {
-	status_block.style.backgroundColor="#ff0";
-	var reader = new FileReader();
-	var xhr = new XMLHttpRequest();
-	var file = document.getElementById("File").files[0];
+	status_block.style.backgroundColor = "#ff0";
+	const reader = new FileReader();
+	const xhr = new XMLHttpRequest();
+	const file = document.getElementById("File").files[0];
 
-	xhr.upload.addEventListener("progress", function(e) {
+	xhr.upload.addEventListener("progress", (e) => {
 		if (e.lengthComputable)
 			ctrl.value = Math.round((e.loaded * 100) / e.total);
 	}, false);
-	xhr.addEventListener("readystatechange", function(e) {
-		if (xhr.readyState == 4)
-			if (xhr.status == 200)
+	xhr.addEventListener("readystatechange", (e) => {
+		if (xhr.readyState === 4)
+			if (xhr.status === 200)
 			{
 				ctrl.value = 100;
 				status_block.style.backgroundColor="#0f0";
@@ -24,14 +24,12 @@ function upload()
 	}, false);
 	xhr.open("POST", "../upload.py?filename=" + encodeURI(file.name));
 	xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
-	reader.onload = function(evt) {
-		xhr.send(evt.target.result);
-	};
+	reader.onload = (evt) => { xhr.send(evt.target.result); };
 	reader.readAsArrayBuffer(file);
 }
 
 function clear_status()
 {
 	ctrl.value = 0;
-	status_block.style.backgroundColor="#f00";
+	status_block.style.backgroundColor = "#f00";
 }
