6a1bbba8c8bde7bf4add4374c9652d13e583ca4c
xukl <xu2k3l4@outlook.com>
2021-09-25 21:50:11 +0800

    对无权限用户增加单文件大小限制、上传文件数限制

--- document-root/upload.py
+++ document-root/upload.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 from urllib.parse import parse_qs, unquote
-from sys import exit, stdin
-from os import getenv
+from sys import stdin
+from os import getenv, listdir
 import re
 
 def response(code, short_msg, msg):
@@ -24,10 +24,19 @@ if '..' in filename or '/' in filename or not filename.isprintable():
 	response(400, 'Bad Request', 'Oops! Invalid file name.')
 	exit()
 
+FILE_ROOT = getenv('FILE_ROOT')
 if getenv('REQUEST_SCHEME') == 'https':
-    filepath = getenv('FILE_ROOT') + '/protected/' + filename
+    filepath = FILE_ROOT + '/protected/' + filename
 else:
-    filepath = getenv('FILE_ROOT') + '/' + filename
+    if len(listdir(FILE_ROOT)) > 6:
+        response(429, 'Too Many Requests',
+                'Can only upload no more than 6 files if unautherized')
+        exit()
+    elif int(getenv('CONTENT_LENGTH')) > 200 * 2**20: # 200MiB
+        response(413, 'Payload Too Large',
+                'Can only upload files no larger than 200MiB if unautherized')
+        exit()
+    filepath = FILE_ROOT + '/' + filename
 
 with open(filepath, 'wb') as f:
 	f.write(stdin.buffer.read())
